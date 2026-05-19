d3ba5bb075cf5b0358b62d40fdf399ce5d885db1
Ola Boss <itzrichie81@gmail.com>
2026-05-19 08:21:59 +0100

    just made few changes on server

--- server.js
+++ server.js
@@ -10,18 +10,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 /**
  * Register API
- const express = require('express');
-const db = require('./database');
-const path = require('path');
-
-const app = express();
-
-// Middleware
-app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public')));
-*/
-/**
- * Register API
  */
 app.post('/register', (req, res) => {
   const { username, password } = req.body;
@@ -61,57 +49,6 @@ app.post('/login', (req, res) => {
   );
 });
 
-// Start server
-app.listen(3000, () => {
-  console.log('Server running at http://localhost:3000');
-});
-app.post('/register', (req, res) => {
-  const { username, password } = req.body;
-
-  db.run(
-    'INSERT INTO users (username, password) VALUES (?, ?)',
-    [username, password],
-    function (err) {
-      if (err) {
-        return res.send('Username already exists');
-      }
-      res.send('Registration successful');
-    }
-  );
-});
-
-/**
- * Login API
- */
-app.post('/login', (req, res) => {
-  const { username, password } = req.body;
-
-  db.get(
-    'SELECT * FROM users WHERE username = ?',
-    [username],
-    (err, user) => {
-      if (!user) {
-        return res.send('User not found');
-      }
-
-      if (user.password !== password) {
-        return res.send('Incorrect password');
-      }
-
-      res.send('Login successful');
-    }
-  );
-});
-
-// Start server
-app.listen(3000, () => {
-  console.log('Server running at http://localhost:3000');
-});
-// Before: const PORT = 3000;
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-console.log('Server running on port', PORT);
-});
 // Start server - FIXED for Render
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
